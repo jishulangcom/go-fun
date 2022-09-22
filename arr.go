25521b9e73f765a4a45baf0d4465fac1c5bb2148
wq <815437877@qq.com>
2022-09-22 21:57:00 +0800

    查找一维数组中某元素的全部index

--- arr.go
+++ arr.go
@@ -5,10 +5,11 @@
 package fun
 
 import (
+	"context"
 	"errors"
 	"reflect"
+	"time"
 )
-
 
 /*
 	【名称:】一维数组求和
@@ -34,7 +35,6 @@ func ArraySum(arr interface{}) (sum float64, err error) {
 	return
 }
 
-
 /*
 	【名称:】变量是否为切片
 	【参数:】变量(interface)
@@ -45,4 +45,160 @@ func IsSlice(val interface{}) bool {
 		return true
 	}
 	return false
+}
+
+// @title: 查找一维数组中某元素的全部index
+// @auth: 技术狼(jishulang.com)
+// @date: 2022/9/22 21:51
+func ArrIndexOfIntAll(arr []int, coNub int, target int, timeout uint) []int {
+	l := len(arr)
+	sp := l / coNub
+	ctx, _ := context.WithCancel(context.Background())
+	ch := make(chan int)
+
+	for i := 0; i < coNub; i++ {
+		go func(i int, ctx context.Context) {
+			start := i * sp
+			curArr := arr[i*sp : (i+1)*sp]
+			for k, v := range curArr {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+
+				if v == target {
+					index := start + k
+					ch <- index
+				}
+			}
+		}(i, ctx)
+	}
+
+	indexArr := []int{}
+	select {
+	case index := <-ch:
+		indexArr = append(indexArr, index)
+	case <-time.After(time.Duration(timeout/1000) * time.Second):
+
+	}
+
+	return indexArr
+}
+
+// @title: 查找一维数组中某元素的一个index
+// @auth: 技术狼(jishulang.com)
+// @date: 2022/9/22 21:51
+func ArrIndexOfIntOne(arr []int, coNub int, target int, timeout uint) *int {
+	l := len(arr)
+	sp := l / coNub
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := make(chan int)
+
+	for i := 0; i < coNub; i++ {
+		go func(i int, ctx context.Context) {
+			start := i * sp
+			curArr := arr[i*sp : (i+1)*sp]
+			for k, v := range curArr {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+
+				if v == target {
+					index := start + k
+					ch <- index
+				}
+			}
+		}(i, ctx)
+	}
+
+	select {
+	case index := <-ch:
+		cancel()
+		return &index
+	case <-time.After(time.Duration(timeout) * time.Second):
+
+	}
+
+	return nil
+}
+
+// @title: 查找一维数组中某元素的全部index
+// @auth: 技术狼(jishulang.com)
+// @date: 2022/9/22 21:51
+func ArrIndexOfStringAll(arr []int, coNub int, target int, timeout uint) []int {
+	l := len(arr)
+	sp := l / coNub
+	ctx, _ := context.WithCancel(context.Background())
+	ch := make(chan int)
+
+	for i := 0; i < coNub; i++ {
+		go func(i int, ctx context.Context) {
+			start := i * sp
+			curArr := arr[i*sp : (i+1)*sp]
+			for k, v := range curArr {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+
+				if v == target {
+					index := start + k
+					ch <- index
+				}
+			}
+		}(i, ctx)
+	}
+
+	indexArr := []int{}
+	select {
+	case index := <-ch:
+		indexArr = append(indexArr, index)
+	case <-time.After(time.Duration(timeout/1000) * time.Second):
+
+	}
+
+	return indexArr
+}
+
+// @title: 查找一维数组中某元素的一个index
+// @auth: 技术狼(jishulang.com)
+// @date: 2022/9/22 21:51
+func ArrIndexOfStringOne(arr []int, coNub int, target int, timeout uint) *int {
+	l := len(arr)
+	sp := l / coNub
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := make(chan int)
+
+	for i := 0; i < coNub; i++ {
+		go func(i int, ctx context.Context) {
+			start := i * sp
+			curArr := arr[i*sp : (i+1)*sp]
+			for k, v := range curArr {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+
+				if v == target {
+					index := start + k
+					ch <- index
+				}
+			}
+		}(i, ctx)
+	}
+
+	select {
+	case index := <-ch:
+		cancel()
+		return &index
+	case <-time.After(time.Duration(timeout) * time.Second):
+
+	}
+
+	return nil
 }
