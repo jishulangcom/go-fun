f12230d49773beb45bbd5de3e1c3dada3fff3941
wq <815437877@qq.com>
2022-09-22 22:01:04 +0800

    Interface To Float64

--- float.go
+++ float.go
@@ -6,11 +6,11 @@ package fun
 
 import (
 	"encoding/binary"
-	"errors"
+	"fmt"
 	"math"
 	"strconv"
+	"strings"
 )
-
 
 /*
 	【名称:】变量是否浮点类型
@@ -21,7 +21,6 @@ import (
 func IsFloat(variable interface{}) bool {
 	return IsDouble(variable)
 }
-
 
 /*
 	【名称:】变量是否浮点类型
@@ -37,72 +36,28 @@ func IsDouble(variable interface{}) bool {
 	return false
 }
 
-
-/*
-	【名称:】转换为 Float64
-	【参数:】val(interface)
-	【返回:】和(float64)，err(error)
-	【备注:】
-*/
-func ToFloat64(val interface{}) (f64 float64, err error) {
-	switch value := val.(type) {
-	case float64:
-		return value, nil
-	case float32:
-		return float64(value), nil
-	case uint8:
-		return float64(value), nil
-	case uint16:
-		return float64(value), nil
-	case uint32:
-		return float64(value), nil
-	case uint64:
-		return float64(value), nil
-	case uint:
-		if strconv.IntSize == 32 || strconv.IntSize == 64 {
-			return float64(value), nil
-		}
-		return 0, errors.New("uint转换为float64失败")
-	case int8:
-		return float64(value), nil
-	case int16:
-		return float64(value), nil
-	case int32:
-		return float64(value), nil
-	case int64:
-		return float64(value), nil
-	case int:
-		if strconv.IntSize == 32 || strconv.IntSize == 64 {
-			return float64(value), nil
-		}
-		return 0, errors.New("int转换为float64失败")
-	case bool:
-		if value {
-			f64 = 1
-		}
-		return
-	case string:
-		f64, err = strconv.ParseFloat(value, 64)
-		if err == nil {
-			return
-		}
-		return 0, errors.New("字符串转换为float64失败")
-	default:
-		return 0, errors.New("转换为float64失败")
-	}
-}
-
-
 /*
 	【名称:】float64转string
 	【参数:】float
 	【返回:】string
 	【备注:】
 */
-func Float64ToStr(float64 float64) string {
-	return strconv.FormatFloat(float64, 'E', -1, 64)
+func Float64ToStr(f float64) string {
+	//fmt.Println("Float64ToStr:", f)
+	//str := fmt.Sprintf("%f", f)
+	//fmt.Println(str)
+	//return fmt.Sprintf("%f", f)
+	return strconv.FormatFloat(f, 'E', -1, 64)
 }
 
+func Float64Decimal(f float64) int {
+	numstr := fmt.Sprint(f)
+	tmp := strings.Split(numstr, ".")
+	if len(tmp) <= 1 {
+		return 0
+	}
+	return len(tmp[1])
+}
 
 /*
 	【名称:】float32转byte
@@ -117,7 +72,6 @@ func Float32ToByte(float32 float32) []byte {
 	return bytes
 }
 
-
 /*
 	【名称:】float64转byte
 	【参数:】float32
@@ -131,7 +85,6 @@ func Float64ToByte(float64 float64) []byte {
 	return bytes
 }
 
-
 /*
 	【名称:】float64转int64
 	【参数:】float64
@@ -141,7 +94,6 @@ func Float64ToByte(float64 float64) []byte {
 func Float64ToInt64(float64 float64) int64 {
 	return int64(float64)
 }
-
 
 /*
 	【名称:】float64转int
@@ -153,7 +105,6 @@ func Float64ToInt(float64 float64) int {
 	return 0
 }
 
-
 /*
 	【名称:】float32转int64
 	【参数:】float32
@@ -163,7 +114,6 @@ func Float64ToInt(float64 float64) int {
 func Float32ToInt64(float64 float64) int64 {
 	return int64(float64)
 }
-
 
 /*
 	【名称:】float32转int
--- interface.go
+++ interface.go
@@ -2,6 +2,7 @@ package fun
 
 import (
 	"encoding/json"
+	"errors"
 	"strconv"
 )
 
@@ -59,4 +60,59 @@ func InterfaceToStr(val interface{}) string {
 	}
 
 	return str
+}
+
+
+/*
+	【名称:】转换为 Float64
+	【参数:】val(interface)
+	【返回:】和(float64)，err(error)
+	【备注:】
+*/
+func InterfaceToFloat64(val interface{}) (f64 float64, err error) {
+	switch value := val.(type) {
+	case float64:
+		return value, nil
+	case float32:
+		return float64(value), nil
+	case uint8:
+		return float64(value), nil
+	case uint16:
+		return float64(value), nil
+	case uint32:
+		return float64(value), nil
+	case uint64:
+		return float64(value), nil
+	case uint:
+		if strconv.IntSize == 32 || strconv.IntSize == 64 {
+			return float64(value), nil
+		}
+		return 0, errors.New("uint转换为float64失败")
+	case int8:
+		return float64(value), nil
+	case int16:
+		return float64(value), nil
+	case int32:
+		return float64(value), nil
+	case int64:
+		return float64(value), nil
+	case int:
+		if strconv.IntSize == 32 || strconv.IntSize == 64 {
+			return float64(value), nil
+		}
+		return 0, errors.New("int转换为float64失败")
+	case bool:
+		if value {
+			f64 = 1
+		}
+		return
+	case string:
+		f64, err = strconv.ParseFloat(value, 64)
+		if err == nil {
+			return
+		}
+		return 0, errors.New("字符串转换为float64失败")
+	default:
+		return 0, errors.New("转换为float64失败")
+	}
 }
